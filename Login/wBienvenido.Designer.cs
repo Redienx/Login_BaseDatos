1d12c892a4ddc96a7d06006444259719c4e65b07
Redienx <117588167+Redienx@users.noreply.github.com>
2023-05-12 12:05:31 -0500

    Conexion Realizada

---
+++ Login/wBienvenido.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Login
+{
+    partial class fmrBienvenido
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBienvenido = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblBienvenido
+            // 
+            this.lblBienvenido.AutoSize = true;
+            this.lblBienvenido.Font = new System.Drawing.Font("Times New Roman", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienvenido.Location = new System.Drawing.Point(279, 174);
+            this.lblBienvenido.Name = "lblBienvenido";
+            this.lblBienvenido.Size = new System.Drawing.Size(190, 32);
+            this.lblBienvenido.TabIndex = 0;
+            this.lblBienvenido.Text = "BIENVENIDO";
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(302, 271);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(143, 54);
+            this.btnCerrar.TabIndex = 1;
+            this.btnCerrar.Text = "Cerrar Sección";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // fmrBienvenido
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.lblBienvenido);
+            this.Name = "fmrBienvenido";
+            this.Text = "Bienvenido";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBienvenido;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
---
+++ Login/wLogin.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Finisar.SQLite;
+
+namespace Login
+{
+    /// <summary>
+    /// Hecho por Sneider Velasquez Iglesias 
+    /// Este codigo es un login conectado a una base de datos
+    /// </summary>
+    public partial class fmrLogin : Form
+    {
+
+        // Variables para almacenar los datos de registro y login
+        string UsuarioRegistro;
+        string ContrasenaRegistro;
+        string Usuario;
+        string Contrasena;
+
+        // Contador para llevar registro de intentos fallidos de login
+        int contador;
+        public fmrLogin()
+        {
+            InitializeComponent();
+        }
+
+        // Método para mostrar el panel de registro al hacer clic en el botón "Registrar"
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            gbxRegistrar.Visible = true;
+        }
+
+        // Método para registrar un nuevo usuario al hacer clic en el botón "Registrarse"
+        private void btnRegistrarse_Click(object sender, EventArgs e)
+        {
+            //Utilizamos estos tres objetos de SQLite
+            SQLiteConnection conexion_sqlite;
+            SQLiteCommand cmd_sqlite;
+
+            //Crear una nueva conexión de la base de datos
+            conexion_sqlite = new SQLiteConnection("Data Source=dbRegistros_de_Usuario2.db;Version=3;Compress=False;");
+
+            try
+            {
+                //Abriremos la conexión
+                conexion_sqlite.Open();
+            }
+            catch (Exception ex)
+            { 
+                MessageBox.Show("La base de datos no se encuentra en la ruta");
+            }
+            cmd_sqlite = conexion_sqlite.CreateCommand();
+
+            ////El objeto SQLiteCommando va a conocer la consulta de SQL
+            ///cmd_sqlite.CommandText = "CREATE TABLE tbl_Registros(ID integer primary key,Usuario varchar(100), Contraseña varchar(100));";
+
+            ////Ejecutaremos la consulta que hemos creado
+            ///cmd_sqlite.ExecuteNonQuery();
+
+            UsuarioRegistro = txtUsuarioRegistrar.Text;
+            ContrasenaRegistro = txtContrasenaRegistrar.Text;
+            try
+            {
+                //Insertando datos en la tabla
+                cmd_sqlite.CommandText = $"INSERT INTO tbl_Registros(Usuario, Contraseña) VALUES('{UsuarioRegistro}', '{ContrasenaRegistro}')";
+                cmd_sqlite.ExecuteNonQuery();
+            }catch (Exception ex) 
+            {
+                MessageBox.Show("Usuario Existente.");
+            }
+            gbxRegistrar.Visible = false;
+            conexion_sqlite.Close();
+        }
+
+        // Método para validar el usuario y contraseña al hacer clic en el botón "Iniciar"
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+
+            //Utilizamos estos tres objetos de SQLite
+            SQLiteConnection conexion_sqlite;
+            SQLiteCommand cmd_sqlite;
+            SQLiteDataReader datareader_sqlite;
+
+            //Crear una nueva conexión de la base de datos
+            conexion_sqlite = new SQLiteConnection("Data Source=dbRegistros_de_Usuario2.db;Version=3;Compress=True;");
+            try
+            {
+                //Abriremos la conexión
+                conexion_sqlite.Open();
+            }catch (Exception ex) { MessageBox.Show("La base da datos no se encuentra en la ruta."); }
+            cmd_sqlite = conexion_sqlite.CreateCommand();
+
+            Usuario = txtUsuario.Text;
+            Contrasena = txtContrasena.Text;
+
+            cmd_sqlite.CommandText = $"SELECT Usuario, Contraseña FROM tbl_Registros WHERE Usuario = '{Usuario}' AND Contraseña = '{Contrasena}'";
+            datareader_sqlite = cmd_sqlite.ExecuteReader();
+
+            // Si el usuario y contraseña coinciden con los registrados, muestra el formulario de bienvenida
+            if (datareader_sqlite.Read())
+            {
+                fmrBienvenido fmrBienvenido = new fmrBienvenido();
+                fmrBienvenido.Show();
+                contador = 0;
+                txtUsuario.Text = null;
+                txtContrasena.Text = null;
+                conexion_sqlite.Close();
+            }
+            else
+            {
+                contador++;
+                // Si hay menos de 3 intentos fallidos, muestra un mensaje de error
+                if (contador < 3)
+                {
+                    MessageBox.Show("Usuario o Contraseña incorrecto. Vuelva a intentar");
+                }
+                // Si hay 3 o más intentos fallidos, muestra un mensaje de error y cierra el formulario de login
+                else
+                {
+                    MessageBox.Show("Muchos intentos erroneos. Bloqueado");
+                    conexion_sqlite.Close();
+                    this.Close();
+                }
+            }
+        }
+    }
+}
